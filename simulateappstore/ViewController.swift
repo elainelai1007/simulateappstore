7c4732d67708b95e37cc48147bcb192bd3ddf84e
Elaine <elaine@musedp.com>
2021-11-24 11:20:31 +0800

    Initial Commit

---
+++ simulateappstore/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  simulateappstore
+//
+//  Created by Elaine Lai on 2021/11/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
